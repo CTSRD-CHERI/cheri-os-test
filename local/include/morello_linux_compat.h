f69c8e0e5c8f858203c320e852bf42387b791cca
Paul Metzger <paul.metzger@cl.cam.ac.uk>
2025-11-10 14:18:19 +0000

    Rename utils.h to morello_linux_compat.h

--- local/include/morello_linux_compat.h
+++ local/include/morello_linux_compat.h
@@ -1,7 +1,7 @@
 /*-
  * SPDX-License-Identifier: BSD-3-Clause
  *
- * Copyright (c) 1982, 1986, 1989, 1993
+ * Copyright (c) 1982, 1986, 1989, 1991, 1993, 1994
  *	The Regents of the University of California.  All rights reserved.
  * (c) UNIX System Laboratories, Inc.
  * All or some portions of this file are derived from material licensed
