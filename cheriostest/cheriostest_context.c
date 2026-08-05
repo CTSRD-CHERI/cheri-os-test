CTSRD-CHERI/cheri-os-test

commit b6858193b7e731bc5b133d2149b6ec4034e1b170

Report getcontext(), setcontext(), etc as not supported on musl

cheriostest/cheriostest_context.c
@@ -30,12 +30,6 @@
  * SUCH DAMAGE.
  */
 
-/*
- * Morello and CHERI Linux are based on Musl libc which doesn't implement the
- * getcontext(), setcontext(), etc system calls. Likely because they were
- * deprecated by POSIX in 2004.
- */
-#ifdef __FreeBSD__
 
 #include <sys/mman.h>
 
@@ -46,6 +40,27 @@
 #include <ucontext.h>
 
 #include "cheriostest.h"
+
+#if defined(__musl_libc_heuristic__)
+/*
+ * Morello and CHERI Linux are based on Musl libc which doesn't implement the
+ * getcontext(), setcontext(), etc system calls. Likely because they were
+ * deprecated by POSIX in 2004.
+ */
+#define	GET_AND_SETCONTEXT_FAILURE_MESSAGE "getcontext() and related " \
+	"functions are not supported by musl libc"
+
+#else
+#define	HAS_GET_AND_SETCONTEXT
+#define	SWAPCONTEXT_ARG1	0x53574150
+
+static int swapcontext_arg1;
+
+static void
+swapcontext_func(int arg1)
+{
+	swapcontext_arg1 = arg1;
+}
 
 static void
 ucontext_mmap_stack(ucontext_t *uctx)
@@ -70,38 +85,40 @@ setcontext_func(int arg1, int arg2)
 	CHERIOSTEST_VERIFY(arg2 == SETCONTEXT_ARG2);
 	cheriostest_success();
 }
+#endif
 
 CHERIOSTEST(setcontext_basic, "Check that setcontext works",
+#if HAS_GET_AND_SETCONTEXT
     /*
      * Currently happens to pass for c18n, possibly because makecontext and
      * setcontext calls are done in the same function?
      */
-    .ct_flaky_reason = XFAIL_FLAKY_C18N_CONTEXT)
+    .ct_flaky_reason = XFAIL_FLAKY_C18N_CONTEXT
+#endif
+)
 {
+#if HAS_GET_AND_SETCONTEXT
 	ucontext_t uc;
 
 	CHERIOSTEST_CHECK_SYSCALL(getcontext(&uc));
 	ucontext_mmap_stack(&uc);
 	uc.uc_link = NULL;
 	makecontext(&uc, (void (*)(void))&setcontext_func, 2, SETCONTEXT_ARG1,
-	    SETCONTEXT_ARG2);
+		SETCONTEXT_ARG2);
 	CHERIOSTEST_CHECK_SYSCALL(setcontext(&uc));
 	cheriostest_failure_errx("returned from successful setcontext");
-}
-
-#define	SWAPCONTEXT_ARG1	0x53574150
-
-static int swapcontext_arg1;
-
-static void
-swapcontext_func(int arg1)
-{
-	swapcontext_arg1 = arg1;
+#else
+	cheriostest_failure_errx(GET_AND_SETCONTEXT_FAILURE_MESSAGE);
+#endif
 }
 
 CHERIOSTEST(swapcontext_basic, "Check that swapcontext works",
-    .ct_flaky_reason = XFAIL_FLAKY_C18N_CONTEXT)
+#if HAS_GET_AND_SETCONTEXT
+    .ct_flaky_reason = XFAIL_FLAKY_C18N_CONTEXT
+#endif
+)
 {
+#if HAS_GET_AND_SETCONTEXT
 	ucontext_t uc, uc_link;
 	int ret;
 
@@ -114,5 +131,7 @@ CHERIOSTEST(swapcontext_basic, "Check that swapcontext works",
 	CHERIOSTEST_VERIFY2(ret == 0, "unknown return value from swapcontext");
 	CHERIOSTEST_VERIFY(swapcontext_arg1 == SWAPCONTEXT_ARG1);
 	cheriostest_success();
-}
+#else
+	cheriostest_failure_errx(GET_AND_SETCONTEXT_FAILURE_MESSAGE);
 #endif
+}

cheriostest/cheriostest.h
@@ -67,6 +67,14 @@
 #error "Unsupported OS"
 #endif
 
+#if !defined(__FreeBSD__) && !defined (__GLIBC__)
+/*
+ * Musl libc does not define an identification macro as FreeBSD, glibc and
+ * others do. We therefore use this heuristic to determine if musl libc is used.
+ */
+#define __musl_libc_heuristic__
+#endif
+
 /*
  * We define our own macros for these because they are not portable.
  * Some implementations of CONCAT() do not expand the arguments

commit 7a4793f6ca5df07f19ae82cf2d07bd07e931a300

Fix value of HAS_GET_AND_SETCONTEXT to 0/1
The checks below use #if so an empty macro evalutes to 1.

Fixes: b6858193

cheriostest/cheriostest_context.c
@@ -51,7 +51,7 @@
 	"functions are not supported by musl libc"
 
 #else
-#define	HAS_GET_AND_SETCONTEXT
+#define	HAS_GET_AND_SETCONTEXT 1
 #define	SWAPCONTEXT_ARG1	0x53574150
 
 static int swapcontext_arg1;